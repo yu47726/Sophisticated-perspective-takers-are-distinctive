6ace5f13346ebe8c8ed18b08491396a4c978d251
yu47726 <150155038+yu47726@users.noreply.github.com>
2024-09-19 19:12:06 +0800

    Add files via upload

--- Scripts/EyeMove.m
+++ Scripts/EyeMove.m
@@ -10,77 +10,47 @@ end
 
 %% 
 for pair = 1:length(SubPair)
-    %%% data processing
-    % first of pair
-    raw_data1 = RawData{SubPair(pair,1)};
-    Index_row1 = [];
-    for row = 1:size(raw_data1,1)
-        if isequaln(NaN,raw_data1{row,1})
-            Index_row1 = [Index_row1;row];
+%%% data processing
+    Index_row = [];
+    hori_pos = [];
+    verti_pos = [];
+    for i = 1:2
+        raw_data = RawData{SubPair(pair,i)};
+        for row = 1:size(raw_data,1)
+            if isequaln(NaN,raw_data{row,1})
+                Index_row = [Index_row;row];
+            end
+        end
+        %Determine whether the subject's gaze falls within the defined range
+        %horizon
+        for row=1:numel(raw_data(:,1) )
+            if  raw_data{row,1} < 0
+                hori_pos = [hori_pos; row];
+            elseif  raw_data{row,1} > 1280
+                hori_pos = [hori_pos; row];
+            end
+        end
+        clear row
+        % vertical
+        for row=1:numel(raw_data(:,2))
+            if raw_data{row,2}  < 142.4
+                verti_pos = [verti_pos; row];
+            elseif raw_data{row,2} > 932.8
+                verti_pos = [verti_pos; row];
+            end
         end
     end
-    %Determine whether the subject's gaze falls within the defined range
-    %horizon
-    hori_pos1 = [];
-    for row=1:numel(raw_data1(:,1) )
-        if  raw_data1{row,1} < 0
-            hori_pos1 = [hori_pos1; row];
-        elseif  raw_data1{row,1} > 1280
-            hori_pos1 = [hori_pos1; row];
-        end
-    end
-    clear row
-    % vertical
-    verti_pos1 = [];
-    for row=1:numel(raw_data1(:,2))
-        if raw_data1{row,2}  < 142.4
-            verti_pos1 = [verti_pos1; row];
-        end
-        if raw_data1{row,2} > 932.8
-            verti_pos1 = [verti_pos1; row];
-        end
-    end
-    
-    
-    % second of pair
-    raw_data2 = RawData{SubPair(pair,2)};
-    Index_row2 = [];
-    for row = 1:size(raw_data2,1)
-        if isequaln(NaN,raw_data2{row,1})
-            Index_row2 = [Index_row2;row];
-        end
-    end
-    
-    hori_pos2 = [];
-    for row=1:numel(raw_data2(:,1) )
-        if raw_data2{row,1} < 0
-            hori_pos2 = [hori_pos2; row];
-        elseif  raw_data1{row,1} > 1280
-            hori_pos2 = [hori_pos2; row];
-        end
-        
-    end
-    clear row
-    verti_pos2 = [];
-    for row=1:numel(raw_data2(:,2) )
-        if raw_data2{row,2}  < 142.4
-            verti_pos2 = [verti_pos2; row];
-        end
-        if raw_data2{row,2} > 932.8
-            verti_pos2 = [verti_pos2; row];
-        end
-    end
+    Null_Row{pair,1}  = [Index_row;verti_pos;hori_pos];  
     
     % delete the data out of defined range
-    Null_Row{pair,1}  = [Index_row1;Index_row2;verti_pos1;verti_pos2;hori_pos1;hori_pos2];    
-    raw_data1(Null_Row{pair,1},:) = [];
-    raw_data2(Null_Row{pair,1},:) = [];
-    
+    data = RawData(SubPair(pair,:));
+    for i = 1:2
+        data{i}(Null_Row{pair},:) = [];
+    end
     % inter-subject dissimilarity of eye-gaze trajectories
-    ISD_x{pair,1} = 1-corr(cell2mat(raw_data1(:,1)),cell2mat(raw_data2(:,1)));
-    ISD_y{pair,1} = 1-corr(cell2mat(raw_data1(:,2)),cell2mat(raw_data2(:,2)));
+    ISD_x{pair,1} = 1-corr(cell2mat(data{1}(:,1)),cell2mat(data{2}(:,1)));
+    ISD_y{pair,1} = 1-corr(cell2mat(data{1}(:,2)),cell2mat(data{2}(:,2)));
+    ISD_mean{pair,1} = mean([ISD_x{pair},ISD_y{pair}]);
     
-    ISD_mean{pair,1} = mean([ISD_x{pair,1},ISD_y{pair,1}]);
-       
-    fprintf(['\n \\ ',PairNam{pair},' done. /\n\n']);
+    fprintf(['\n \\ ',PairNam{pair},' Processing done. /\n\n']);
 end
